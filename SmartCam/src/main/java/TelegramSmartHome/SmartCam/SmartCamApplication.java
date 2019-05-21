3651a4179f4c359477f8cf8464e00b9c7bf4334e
Christian Laussmann <ch.laussmann@gmail.com>
2019-05-21 17:05:28 +0200

    SETUP

--- SmartCam/src/main/java/TelegramSmartHome/SmartCam/SmartCamApplication.java
+++ SmartCam/src/main/java/TelegramSmartHome/SmartCam/SmartCamApplication.java
@@ -1,13 +1,9 @@
 package TelegramSmartHome.SmartCam;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
 public class SmartCamApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SmartCamApplication.class, args);
+		System.out.println("Hello");
 	}
 
 }
