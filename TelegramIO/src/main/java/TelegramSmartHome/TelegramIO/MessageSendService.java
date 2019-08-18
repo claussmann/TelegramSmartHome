fe0dec214d8e8f0fc6a97a85262f7990d9edc65a
Jonathan Bobak <jonathan.bobak@yahoo.de>
2019-08-18 15:35:58 +0200

    ADD: sendMessage for MessageSendService

--- TelegramIO/src/main/java/TelegramSmartHome/TelegramIO/MessageSendService.java
+++ TelegramIO/src/main/java/TelegramSmartHome/TelegramIO/MessageSendService.java
@@ -1,4 +1,16 @@
 package TelegramSmartHome.TelegramIO;
 
+import TelegramSmartHome.TelegramIO.HttpsApiComm.HttpsHandler;
+
 public class MessageSendService {
+
+    private final HttpsHandler httpsHandler;
+
+    public MessageSendService(HttpsHandler httpsHandler) {
+        this.httpsHandler = httpsHandler;
+    }
+
+    public void sendMessage(long chatId, String message) {
+        httpsHandler.httpsPushRequest(chatId, message);
+    }
 }
