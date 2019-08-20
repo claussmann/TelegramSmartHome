ea973b8ae6ac0f652ccf7fab9d8a658198755c72
Christian Laussmann <ch.laussmann@gmail.com>
2019-08-20 20:53:29 +0200

    REFACTOR: Cleaner and easy to read code

--- SmartHome/src/main/java/TelegramSmartHome/SmartHome/Config/Config.java
+++ SmartHome/src/main/java/TelegramSmartHome/SmartHome/Config/Config.java
@@ -1,87 +1,34 @@
 package TelegramSmartHome.SmartHome.Config;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import io.vavr.control.Try;
 
 import java.io.*;
-import java.util.Scanner;
 
 public class Config {
     private String configFile = "config.conf"; //TODO:set location to default (/etc/TelegramSmartHome/config) except for developer
 
     private ConfigFile conf;
 
-    public Config(){
-        conf = new ConfigFile();
-        manageConfig();
-    }
-
-    private void manageConfig() {
-        String json=readConfigFile();
-        boolean newConfig = false;
+    public Config() {
+        String json = Try.of(() -> readConfigFile()).getOrElse("");
         ObjectMapper mapper = new ObjectMapper();
-
-        try {
-            conf = mapper.readValue(json, ConfigFile.class);
-            System.out.println("Configuration loaded successfully");
-        } catch (IOException e) {
-            createNewConfig();
-            newConfig = true;
-        }
-        if(!newConfig) {
-            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-            System.out.println("Edit Config?(y/n)");
-            String answer = "n";
-            try {
-                answer = br.readLine();
-                answer = answer.toLowerCase();
-                if(answer.length()>1 || !(answer.equals("y") || answer.equals("n"))) {
-                    throw new IllegalArgumentException("Input not y or n");
-                }
-            } catch (IOException e) {
-                System.out.println("Invalid answer");
-                e.printStackTrace();
-            } catch (IllegalArgumentException e) {
-                e.printStackTrace();
-            }
-            if(answer.equals("y")){
-                editConfig();
-            }
-
-        }
-
+        conf = Try.of(() -> mapper.readValue(json, ConfigFile.class))
+                .getOrElse(createNewConfig());
     }
 
-    private void editConfig() {
-        System.out.println("Current Bot Token:");
-        System.out.println(conf.botToken);
-        System.out.println("New Bot Token:");
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        try {
-            String token = br.readLine();
-            conf.setBotToken(token);
-            System.out.println("New token saved");
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        saveConfig();
-    }
-
-    private String readConfigFile(){
-        BufferedReader reader;
-        String ret="";
-        try {
-            reader = new BufferedReader(new FileReader(configFile));
-            String line;
-            while((line = reader.readLine())!=null){
-                ret+=line;
-            }
-        } catch (Exception e) {
-            System.out.println("No Config found!");
+    private String readConfigFile() throws Exception {
+        String ret = "";
+        BufferedReader reader = new BufferedReader(new FileReader(configFile));
+        String line;
+        while ((line = reader.readLine()) != null) {
+            ret += line;
         }
         return ret;
     }
 
-    private void createNewConfig() {
+    private ConfigFile createNewConfig() {
+        ConfigFile newConf = new ConfigFile();
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         System.out.println("Creating new Config-File");
         System.out.println("Insert your Bot Token:");
@@ -92,27 +39,31 @@ public class Config {
         } catch (IOException e) {
             e.printStackTrace();
         }
-        conf.setBotToken(token);
-        conf.setLastMessage(0);
-
-        saveConfig();
+        newConf.setBotToken(token);
+        newConf.setLastMessage(0);
+        saveConfig(newConf);
+        return newConf;
     }
 
     private void saveConfig() {
+        saveConfig(conf);
+    }
+
+    private void saveConfig(ConfigFile configFile) {
         ObjectMapper objectMapper = new ObjectMapper();
         try {
-            objectMapper.writeValue(new FileOutputStream("config.conf"), conf);
+            objectMapper.writeValue(new FileOutputStream("config.conf"), configFile);
         } catch (IOException e) {
             e.printStackTrace();
         }
-        System.out.println("Successfully saved Configuration (see config.conf)");
+        System.out.println("Successfully saved Configuration (see "+this.configFile+")");
     }
 
-    public String getBotToken(){
+    public String getBotToken() {
         return conf.botToken;
     }
 
-    public int getLastMessageID(){
+    public int getLastMessageID() {
         return conf.lastMessage;
     }
 
